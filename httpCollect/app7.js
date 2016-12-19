a30a25b4180f67b5e889595731bbf98a60d82202
Camilo Giraldo <camilogiraldo91@gmail.com>
2016-12-18 20:55:38 -0500

    Http Collect finish

---
+++ httpCollect/app7.js
@@ -0,0 +1,22 @@
+var fs = require('fs'); //Require FileSystem
+var http = require('http');
+var bl = require('bl');
+if (process.argv[2]) {   //If an argument is passed as a param through console
+  var url = process.argv[2];
+
+  http.get(url, function(res) { //Issue an http request to url passed as a parameter the callback res
+    res.pipe(bl(function(err, data){
+      if (err)
+       console.log(err);
+
+      console.log(data.toString().length);
+      console.log(data.toString());
+
+    }));
+
+
+  });
+}else {
+  console.log('Not argument found')
+  console.log(process.argv)
+}
